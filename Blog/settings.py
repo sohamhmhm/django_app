71b0266da7c19fc422b9c3baf8fe136154dba0ea
sohamhmhm <142711733+sohamhmhm@users.noreply.github.com>
2024-04-10 23:10:08 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogapp',
+        'USER': 'simran',
+        'PASSWORD': 'Simran25',
+        'HOST': 'blogapp.ccm0daaaeive.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
